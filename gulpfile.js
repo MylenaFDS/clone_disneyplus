9102c360ccbd19b0666112e44b14154f2ea2471b
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-04-24 22:38:15 -0300

    Inclusão de main.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+//Importações:
 
-let imageCount = 0; // Inicializa o contador de imagens
+const gulp = require('gulp'); // Importa o pacote Gulp para automatização de tarefas
+const sass = require('gulp-sass')(require('sass')); // Importa o plugin Gulp Sass para compilar arquivos SCSS
+const imagemin = require('gulp-imagemin'); // Importa o plugin Gulp Imagemin para otimizar imagens
+
+//Funções:
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/styles/*.scss') // Seleciona todos os arquivos SCSS na pasta styles
+        .pipe(sass({ outputStyle: 'compressed' })) // Compila os arquivos SCSS para CSS comprimido
+        .pipe(gulp.dest('./dist/css')); // Salva os arquivos compilados na pasta css dentro de dist
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+    return gulp.src('./src/images/**/*') // Seleciona todas as imagens na pasta images e suas subpastas
+        .pipe(imagemin()) // Otimiza as imagens
+        .pipe(gulp.dest('./dist/images')); // Salva as imagens otimizadas na pasta images dentro de dist
 }
 
-exports.default = gulp.parallel(styles, images);
+//Exportações:
+
+exports.default = gulp.parallel(styles, images); // Define a tarefa padrão que executa as funções styles e images em paralelo
 
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Monitora mudanças nos arquivos SCSS e executa a função styles
 }
